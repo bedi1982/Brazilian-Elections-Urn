adf211f6e419a5990a55bdcb7f498b079bd7fc05
Sergio Rafael Lemke <bedi.com@gmail.com>
2015-03-18 23:01:42 -0300

    Added the C++ code itself to the Qtinterface

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "QDebug"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -8,68 +9,218 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
 }
 
+bool s1, s2 = false;
+int n1,n2 = 0;
+int count = 0;
+
 MainWindow::~MainWindow()
 {
     delete ui;
 }
 
+void MainWindow::on_btn_confirma_clicked()
+{
+    count++;
+    s1 = false;
+    s2 = false;
+    block_buttons(true);
+    ui->btn_confirma->setDisabled(true);
+    ui->btn_branco->setDisabled(true);
+    ui->btn_corrige->setDisabled(true);
+    ui->radioButton->setChecked(true);
+
+    setPresident();
+    if(n1 == 1 && n2 == 3 ){
+        ui->lbl_log->setText("Voto em Dilma registrado na EPROM.");
+    }else{
+        if(n1 == 5 && n2 == 1 ){
+            ui->lbl_log->setText("Voto em Molusco registrado na EPROM.");
+        }else{
+            if(count % 2 == 0){
+                ui->lbl_log->setText("Voto em Dilma registrado na EPROM.");
+            }else{
+                ui->lbl_log->setText("Voto em Aecio registrado na EPROM.");
+            }
+        }
+    }
+}
+
+
+void MainWindow::on_btn_corrige_clicked()
+{
+    //QPixmap foto(":/images/aecio.jpg");
+    ui->lbl_foto_presidente->setText(" ");
+    ui->lbl_lula_penquin->setText(" ");
+    ui->lbl_nome_presidente->setText("");
+    ui->lbl_nome_partido->setText("");
+    ui->lbl_numero1_president->setText("");
+    ui->lbl_numero2_president->setText("");
+
+    s1 = false;
+    s2 = false;
+    block_buttons(false);
+}
+
+void MainWindow::setPresident()
+{
+    if(n1 == 4 && n2 == 5){
+        QPixmap foto(":/images/aecio.jpg");
+        ui->lbl_foto_presidente->setPixmap(foto);
+        ui->lbl_nome_presidente->setText("Aecio");
+        ui->lbl_nome_partido->setText("PSDB");
+    }
+
+    if(n1 == 1 && n2 == 3){
+        QPixmap foto(":/images/dilma.jpg");
+        ui->lbl_foto_presidente->setPixmap(foto);
+        ui->lbl_nome_presidente->setText("Dilma");
+        ui->lbl_nome_partido->setText("PT");
+    }
+
+    if(n1 ==5 && n2 == 1){
+        QPixmap foto(":/images/lula.jpg");
+        ui->lbl_foto_presidente->setPixmap(foto);
+        ui->lbl_nome_presidente->setText("Lula");
+        ui->lbl_nome_partido->setText("PT");
+        QPixmap foto2(":/images/pic_cachaca51.png");
+        ui->lbl_lula_penquin->setPixmap(foto2);
+    }
+}
+
+void MainWindow::set_number(int numero){
+    if(s1 == false){
+        ui->lbl_numero1_president->setText(QString::number(numero));
+        n1 = numero;
+        s1 = true;
+
+        if(s1 == true && s2 == true){
+            block_buttons(true);
+        }
+        return;
+    }
+    if(s2 == false){
+        ui->lbl_numero2_president->setText(QString::number(numero));
+        n2 = numero;
+        s2 = true;
+
+        if(s1 == true && s2 == true){
+            block_buttons(true);
+        }
+        return;
+    }
+}
+
+void MainWindow::block_buttons(bool set){
+    ui->btn_00->setDisabled(set);
+    ui->btn_11->setDisabled(set);
+    ui->btn_22->setDisabled(set);
+    ui->btn_33->setDisabled(set);
+    ui->btn_44->setDisabled(set);
+    ui->btn_55->setDisabled(set);
+    ui->btn_66->setDisabled(set);
+    ui->btn_77->setDisabled(set);
+    ui->btn_88->setDisabled(set);
+    ui->btn_99->setDisabled(set);
+}
+
+void MainWindow::on_btn_00_clicked()
+{
+    set_number(0);
+        if(s1 == true && s2 == true){
+            setPresident();
+    }
+}
+
+void MainWindow::on_btn_11_clicked()
+{
+    set_number(1);
+    if(s1 == true && s2 == true){
+        setPresident();
+}
+}
+
+void MainWindow::on_btn_22_clicked()
+{
+    set_number(3);
+    if(s1 == true && s2 == true){
+        setPresident();
+}
+}
+
 void MainWindow::on_btn_33_clicked()
 {
-        if(true){
-                ui->lbl_numero1_president->setText("3");
-        }else{
-                ui->lbl_numero2_president->setText("3");
-        }
+    set_number(3);
+    if(s1 == true && s2 == true){
+        setPresident();
+}
 }
 
 void MainWindow::on_btn_44_clicked()
 {
-    ui->lbl_numero1_president->setText("4");
+    set_number(4);
+    if(s1 == true && s2 == true){
+        setPresident();
+}
 }
 
 void MainWindow::on_btn_55_clicked()
 {
-    ui->lbl_numero1_president->setText("5");
-}
-
-void MainWindow::on_btn_confirma_clicked()
-{
-    QPixmap foto(":/images/dilma.jpg");
-    ui->lbl_foto_presidente->setPixmap(foto);
-    ui->lbl_foto_presidente->setPixmap(foto);
-    ui->lbl_nome_presidente->setText("Dilma");
-    ui->lbl_nome_partido->setText("PT");
-    ui->lbl_numero1_president->setText("1");
-    ui->lbl_numero2_president->setText("3");
-}
-
-void MainWindow::on_btn_corrige_clicked()
-{
-    QPixmap foto(":/images/aecio.jpg");
-    ui->lbl_foto_presidente->setPixmap(foto);
-    ui->lbl_foto_presidente->setPixmap(foto);
-    ui->lbl_nome_presidente->setText("Aecio");
-    ui->lbl_nome_partido->setText("PSDB");
-    ui->lbl_numero1_president->setText("4");
-    ui->lbl_numero2_president->setText("5");
-}
-
-void MainWindow::setPresident(int num)
-{
-    if( num = 45){
-        QPixmap foto(":/images/aecio.jpg");
-
-    }
-
-    if(num = 13){
-        QPixmap foto(":/images/dilma.jpg");
-
+        set_number(5);
+        if(s1 == true && s2 == true){
+            setPresident();
     }
 }
 
-int check_position(int a){
-    if(a = 0){
-        return 0;
-    }
-    return 1;
+void MainWindow::on_btn_66_clicked()
+{
+    set_number(6);
+    if(s1 == true && s2 == true){
+        setPresident();
+}
+}
+
+void MainWindow::on_btn_77_clicked()
+{
+    set_number(7);
+    if(s1 == true && s2 == true){
+        setPresident();
+}
+}
+
+void MainWindow::on_btn_88_clicked()
+{
+    set_number(8);
+    if(s1 == true && s2 == true){
+        setPresident();
+}
+}
+
+void MainWindow::on_btn_99_clicked()
+{
+    set_number(9);
+    if(s1 == true && s2 == true){
+        setPresident();
+}
+}
+
+void MainWindow::on_radioButton_clicked()
+{
+    block_buttons(false);
+    ui->btn_confirma->setDisabled(false);
+    ui->btn_branco->setDisabled(false);
+    ui->btn_corrige->setDisabled(false);
+
+
+    ui->lbl_foto_presidente->setText(" ");
+    ui->lbl_lula_penquin->setText(" ");
+    ui->lbl_nome_presidente->setText("");
+    ui->lbl_nome_partido->setText("");
+    ui->lbl_numero1_president->setText("");
+    ui->lbl_numero2_president->setText("");
+    ui->lbl_log->setText("");
+
+    s1 = false;
+    s2 = false;
+    block_buttons(false);
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -16,16 +16,24 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_btn_55_clicked();
-    void on_btn_44_clicked();
+    void on_btn_00_clicked();
+    void on_btn_11_clicked();
+    void on_btn_22_clicked();
     void on_btn_33_clicked();
+    void on_btn_44_clicked();
+    void on_btn_55_clicked();
+    void on_btn_66_clicked();
+    void on_btn_77_clicked();
+    void on_btn_88_clicked();
+    void on_btn_99_clicked();
 
     void on_btn_confirma_clicked();
-
-    void setPresident(int num);
-
-
+    void setPresident();
     void on_btn_corrige_clicked();
+    void set_number(int n1);
+    void block_buttons(bool set);
+
+    void on_radioButton_clicked();
 
 private:
     Ui::MainWindow *ui;
